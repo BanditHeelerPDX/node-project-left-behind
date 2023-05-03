73b0926d77b943028c63b297fb1494b3eeec67f0
2code4wsp <tlynde1043@gmail.com>
2023-05-02 21:03:54 -0700

    deleted link routes

--- models/index.js
+++ models/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const bookmarkRoutes = require('./bookmarkRoutes');
-const linksRoutes = require('./linksRoutes');
 const notesRoutes = require('./userRoutes');
 const videoRoutes = require('./videoRoutes');
 
 router.use('/bookmarks', bookmarkRoutes);
-router.use('/links', linksRoutes);
 router.use('/notes', notesRoutes);
 router.use('users', userRoutes);
 router.use('/videos', videoRoutes);
