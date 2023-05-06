df5aabee5564b8ec715ddbf78154b7565260ad50
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-05-05 17:39:53 -0700

    started work on home route - got pulled away

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,50 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { Note, Bookmark, User } = require("../models");
+const authorized = require("../utils/auth");
 
-const userRoutes = require('./userRoutes');
-
-router.use('/users', userRoutes);
+router.get("/", async (req, res) => {
+  try {
+    const noteData = await Note.findAll({
+      limit: 10,
+      order: [["created_at", "DESC"]],
+      where: {
+        author: req.session.user_id,
+      },
+      include: [
+        {
+          model: Note,
+          attributes: ["id", "title", "content", "author", "created_at"],
+        },
+      ],
+    });
+    const notesVanilla = noteData.map((note) => note.get({ plain: true }));
+    const bookmarks = await Bookmark.findAll({
+      limit: 10,
+      order: [["created_at", "DESC"]],
+      where: {
+        author: req.session.user_id,
+      },
+      include: [
+        {
+          model: Bookmark,
+          attributes: ["id", "title", "content", "author", "created_at"],
+        },
+      ],
+    });
+    const bookmarksVanilla = bookmarks.map((bookmark) =>
+      bookmark.get({ plain: true })
+    );
+    const results = {
+      notes: notesVanilla,
+      bookmarks: bookmarksVanilla,
+    };
+    res.render("homepage", {
+      results,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
