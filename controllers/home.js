0f5f99961697a3e05df7a71f4f18ac614aa1a9e6
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-05-05 22:22:12 -0700

    minimum viable done for home routes

--- controllers/home.js
+++ controllers/home.js
@@ -47,4 +47,46 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get('/notes/:id', authorized, async (req, res) => {
+    try {
+        const noteData = await Note.findByPk(req.params.id, {
+            include: [
+                {
+                     model: Note,
+                     attributes: ['id', 'title', 'content', 'author', 'created_at'],
+                },
+            ],
+        });
+        const note = noteData.get({ plain: true });
+        res.render('note', { ...note, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/bookmarks/:id', authorized, async (req, res) => {
+    try {
+        const bookmarkData = await Bookmark.findByPk(req.params.id, {
+            include: [
+                {
+                        model: Bookmark,
+                        attributes: ['id', 'title', 'content', 'author', 'created_at'],
+                },
+            ],
+        });
+        const bookmark = bookmarkData.get({ plain: true });
+        res.render('bookmark', { ...bookmark, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
 module.exports = router;
