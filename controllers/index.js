f332d1fd338aea166da077716189c3470fae4591
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-05-08 15:35:17 -0700

    cleaning up and completing controller files

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home');
+const dashRoutes = require('./dashboard');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
 
 module.exports = router;
