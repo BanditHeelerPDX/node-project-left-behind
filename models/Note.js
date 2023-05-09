d64439610d1f441b87e3b33bcce617daee2fd63f
2code4wsp <tlynde1043@gmail.com>
2023-05-09 10:59:18 -0700

    added seeds corrected routes

--- models/Note.js
+++ models/Note.js
@@ -1,47 +1,23 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Note extends Model {}
 
 Note.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
+  {
 
-        title: {
-            type: DataTypes.STRING,
-        content: DataTypes.STRING,
-        allowNull: false,
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-        author: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'username',
-            },
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
-        },
-        content: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'note',
-    }
+  },
+  {
+    sequelize,
+  }
 );
 
 module.exports = Note;
